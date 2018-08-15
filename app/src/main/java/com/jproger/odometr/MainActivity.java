1cdea94bc0f072b4974c187d37d69d4776331cce
Jproger <webscripting@ya.ru>
2018-08-15 16:47:42 +0300

    Add temp random service

--- app/src/main/java/com/jproger/odometr/MainActivity.java
+++ app/src/main/java/com/jproger/odometr/MainActivity.java
@@ -2,10 +2,16 @@ package com.jproger.odometr;
 
 import android.app.Activity;
 import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.content.ServiceConnection;
 import android.media.midi.MidiManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends Activity {
 
@@ -32,5 +38,39 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayDistance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, OdometerService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(bound) {
+            unbindService(connection);
+            bound = false;
+        }
+    }
+
+    private void displayDistance() {
+        final TextView distanceView = (TextView) findViewById(R.id.distance);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                double distance = 0.0;
+                if(bound && odometer != null) {
+                    distance = odometer.getDistance();
+                }
+                String distanceStr = String.format(Locale.getDefault(), "%1$,.2f miles", distance);
+                distanceView.setText(distanceStr);
+                handler.postDelayed(this,1000);
+            }
+        });
     }
 }
